bd56bf8cb7eaeb92cbdf62b6934cd363a57124ac
Maria <malett@ttu.ee>
2018-02-26 11:51:05 +0200

    initial project

---
+++ src/main/java/IDK0071/ttu/Wakepark_back/WakeparkBackApplication.java
@@ -0,0 +1,12 @@
+package IDK0071.ttu.Wakepark_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakeparkBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WakeparkBackApplication.class, args);
+	}
+}
